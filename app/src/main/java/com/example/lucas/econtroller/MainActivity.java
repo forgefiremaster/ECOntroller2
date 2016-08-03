0796d5c0417e0e936684f1c9420173c9ff47c398
lucas fernandes <lucas fernandes>
2016-08-03 20:31:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lucas/econtroller/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lucas.econtroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void textUpload(){
+        int i = 1 + 1;
+    }
+}
